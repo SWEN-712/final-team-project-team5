5aec0b7904716716d3208d25e9e82b702bf2903b
Ben Dow <benjamindow1@gmail.com>
2020-04-17 14:10:38 -0400

    ui work

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,4 +1,5 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log("Background got a message!")
+    console.log(message);
     sendResponse({})
 })
--- src/ui/popup.tsx
+++ src/ui/popup.tsx
@@ -4,10 +4,44 @@ import * as ReactDOM from "react-dom"
 import "../styles/popup.css"
 
 class Hello extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            smoothingEnabled : false,
+            smoothingAmount : 0
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+
+    }
+
+    log(message){
+       chrome.runtime.sendMessage(message)
+    }
+
+    handleCheckboxChange(event){
+        this.setState({[event.target.name] :  !this.state['smoothingEnabled']});
+    }
+
+    handleChange(event){
+        this.setState({[event.target.name] : (event.target.value)});
+    }
+
+
+
     render() {
         return (
             <div className="popup-padded">
-                <h1>{ chrome.i18n.getMessage("l10nHello") }</h1>
+                <form>
+                    <label><h3>Mouse Smoothing</h3></label>
+                    <label>Enable:</label>
+                    <input type="checkbox" name="smoothingEnabled" checked={this.state["smoothingEnabled"]} onChange={this.handleCheckboxChange}/>
+                    <br/>
+                    <label> Smoothing Amount: {this.state["smoothingAmount"]}</label>
+                    <input type="range" id="smoothingAmount" name="smoothingAmount" step="1" min = "0" max="100%" value={this.state["smoothingAmount"]} onChange={this.handleChange} readOnly={!this.state['smoothingEnabled']}/>
+                </form>
             </div>
         )
     }
